cd1ad58260a270f6943607f73e594d1a55a82df9
ken1286 <kindle1286@gmail.com>
2019-10-15 16:12:56 -0700

    working on LRU cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.storage = {}
+        self.order = DoublyLinkedList()
+        self.size = 0
+        self.limit = limit
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +23,14 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+
+        if key in self.storage and self.size > 0:
+            get_node = self.storage[key]  # node = value
+            self.order.add_to_head({key: get_node})
+            return get_node
+        return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +42,11 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        self.storage[key] = value
+        self.order.add_to_head({key: value})
+        self.size += 1
+        if self.size > self.limit:
+            self.order.remove_from_tail()
+            self.size -= 1
