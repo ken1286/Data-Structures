ade190499be2ff60794aa30dba57c45e5e531838
ken1286 <kindle1286@gmail.com>
2019-10-15 16:13:23 -0700

    LRU cache partially done

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -4,7 +4,7 @@ from doubly_linked_list import DoublyLinkedList
 # sys.path.append('../doubly_linked_list')
 
 
-class Queue:
+class Queue:  # FIFO
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -3,20 +3,24 @@ from doubly_linked_list import DoublyLinkedList
 # sys.path.append('../doubly_linked_list')
 
 
-class Stack:
+class Stack:  # FILO
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Same because we are adding and removing from the end
         self.storage = DoublyLinkedList()
 
     def push(self, value):
         self.storage.add_to_tail(value)
+        self.size = self.storage.__len__()
 
     def pop(self):
         if not self.storage.head:
             return None
-        return self.storage.remove_from_tail()
+        value = self.storage.tail.value
+        self.storage.remove_from_tail()
+        self.size = self.storage.__len__()
+        return value
 
     def len(self):
-        return self.storage.__len__()
+        return self.size
